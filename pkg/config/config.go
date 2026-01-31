6d2fbe33e490f88ad522aa6c891290a2f379a883
Alejandro Aldana <baker-plan-b@hotmail.com>
2026-01-31 17:20:46 -0600

    Add missing config vars

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -9,6 +9,9 @@ import (
 type Config struct {
 	HTTPPort     int
 	DBConnString string
+	RedisAddr    string
+	RedisPass    string
+	RedisDB      int
 	LogLevel     string
 	PProfEnabled bool
 }
@@ -17,10 +20,19 @@ type Config struct {
 func Load() *Config {
 	port, _ := strconv.Atoi(getEnv("HTTP_PORT", "8080"))
 	pprof, _ := strconv.ParseBool(getEnv("PPROF_ENABLED", "true"))
+	dbConnString := getEnv("DATABASE_URL", "postgres://postgres:postgres@localhost:5432/tucredito?sslmode=disable")
+	redisAddr := getEnv("REDIS_ADDR", "localhost:6379")
+	redisPass := getEnv("REDIS_PASSWORD", "")
+	redisDB, _ := strconv.Atoi(getEnv("REDIS_DB", "0"))
+	level := getEnv("LOG_LEVEL", "info")
+
 	return &Config{
 		HTTPPort:     port,
-		DBConnString: getEnv("DATABASE_URL", "postgres://postgres:postgres@localhost:5432/tucredito?sslmode=disable"),
-		LogLevel:     getEnv("LOG_LEVEL", "info"),
+		DBConnString: dbConnString,
+		RedisAddr:    redisAddr,
+		RedisPass:    redisPass,
+		RedisDB:      redisDB,
+		LogLevel:     level,
 		PProfEnabled: pprof,
 	}
 }
